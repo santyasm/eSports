ff3b403e03d8eafcd0ad19200002d8b3f44b8f51
santyasm <yyasant5@gmail.com>
2023-07-02 18:16:06 -0300

    games

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         fundo: "url('../public/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 18.8%, #43E7AD 23.94%, #e10550 78.57%)',
       },
     },
   },
