2d95f243c9aa5bc6fdac7c299828af64a8182460
santyasm <yyasant5@gmail.com>
2023-07-04 14:34:00 -0300

    conexão db com prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json([
-        {id: 1, name: "Anúncio 1"},
-        {id: 2, name: "Anúncio 2"},
-        {id: 3, name: "Anúncio 3"},
-        {id: 4, name: "Anúncio 4"},
+app.get("/games/:gameId/ads", (req, res) => {
+  const { gameId } = req.params;
 
-    ])
-})
-app.listen(4000, () => console.log("App rodando!"));
+  return res.send(gameId);
+  return res.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+    { id: 4, name: "Anúncio 4" },
+  ]);
+});
+
+app.get("/games", (req, res) => {
+    
+});
+
+app.listen(4000, () => console.log("App rodando em localhost:4000"));
