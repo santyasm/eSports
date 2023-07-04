2e6670f30272a8fb4632ba4af4b4e3cdc167ccf4
santyasm <yyasant5@gmail.com>
2023-07-04 10:13:28 -0300

    Build com ts

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json([
+        {id: 1, name: "Anúncio 1"},
+        {id: 2, name: "Anúncio 2"},
+        {id: 3, name: "Anúncio 3"},
+        {id: 4, name: "Anúncio 4"},
+
+    ])
+})
+app.listen(4000, () => console.log("App rodando!"));
