37de9d97bea338266ff20d19ada27d26274f66de
santyasm <yyasant5@gmail.com>
2023-07-04 18:27:38 -0300

    List ads by game

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,40 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const prisma = new PrismaClient({});
-
-app.get("/games/:gameId/ads", (req, res) => {
-  const { gameId } = req.params;
-
-  return res.send(gameId);
+const prisma = new PrismaClient({
+	log: ['query'],
 });
 
-app.get("/games", async (req, res) => {
-  try {
-    const games = await prisma.game.findMany({
-      include: {
-        _count: {
-          select: {
-            ads: true,
-          },
-        },
-      },
-    });
+app.get('/games/:gameId/ads', async (req, res) => {
+	const { gameId } = req.params;
+	const ads = await prisma.ad.findMany({
+		where: {
+			gameId,
+		},
+	});
 
-    return res.json(games);
-  } catch (error) {}
+	return res.send(ads);
 });
 
-app.listen(4000, () => console.log("App rodando em localhost:4000"));
+app.get('/games', async (req, res) => {
+	try {
+		const games = await prisma.game.findMany({
+			include: {
+				_count: {
+					select: {
+						ads: true,
+					},
+				},
+			},
+		});
+
+		return res.json(games);
+	} catch (error) {}
+});
+
+app.post('/ads', (req, res) => {
+	return res.status(201).json([]);
+});
+
+app.listen(4000, () => console.log('App rodando em localhost:4000'));
