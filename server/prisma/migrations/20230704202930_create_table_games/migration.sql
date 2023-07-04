2d9f560c0b2b82e1c9f952a15889fbdf53959b8d
santyasm <yyasant5@gmail.com>
2023-07-04 18:09:52 -0300

    List games with ads

--- server/prisma/migrations/20230704202930_create_table_games/migration.sql
+++ server/prisma/migrations/20230704202930_create_table_games/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `Game` (
     `id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
-    `bannerurl` VARCHAR(191) NOT NULL,
+    `bannerUrl` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,29 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
 const app = express();
+const prisma = new PrismaClient({});
 
 app.get("/games/:gameId/ads", (req, res) => {
   const { gameId } = req.params;
 
   return res.send(gameId);
-  return res.json([
-    { id: 1, name: "Anúncio 1" },
-    { id: 2, name: "Anúncio 2" },
-    { id: 3, name: "Anúncio 3" },
-    { id: 4, name: "Anúncio 4" },
-  ]);
 });
 
-app.get("/games", (req, res) => {
-    
+app.get("/games", async (req, res) => {
+  try {
+    const games = await prisma.game.findMany({
+      include: {
+        _count: {
+          select: {
+            ads: true,
+          },
+        },
+      },
+    });
+
+    return res.json(games);
+  } catch (error) {}
 });
 
 app.listen(4000, () => console.log("App rodando em localhost:4000"));
