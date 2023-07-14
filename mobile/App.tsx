f8f28716f5082b59a318852c40c313a45051efb1
santyasm <yyasant5@gmail.com>
2023-07-13 22:16:47 -0300

    Loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { Background } from './src/components/Background';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter";
+
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+});
+
   return (
     <Background>
-      <StatusBar 
-      />
+      <StatusBar />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
--- mobile/src/components/Background/index.tsx
+++ mobile/src/components/Background/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import { ImageBackground } from 'react-native';
-import {
-    useFonts,
-    Inter_400Regular,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_900Black
-} from "@expo-google-fonts/inter";
 
 import backgroundImg from '../../../assets/background-galaxy.png'
 
@@ -17,12 +10,6 @@ interface Props {
 }
 
 export function Background({ children }: Props) {
-    const [fontsLoaded] = useFonts({
-        Inter_400Regular,
-        Inter_600SemiBold,
-        Inter_700Bold,
-        Inter_900Black
-    });
 
     return (
         <ImageBackground style={styles.container}
